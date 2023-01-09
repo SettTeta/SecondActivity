a3d1a22e564c6d6b4b3b2f785b568fbdd44d6351
Sett Teta <stetanonsakul@gmail.com>
2023-01-09 19:46:49 +0700

    Second Activity Code

---
+++ app/src/main/java/settteta/secondactivity/SecondActivity.kt
@@ -0,0 +1,23 @@
+package settteta.secondactivity
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val displayName = intent.getStringExtra("NAME")
+        displayTextView.text = "Hello $displayName"
+
+        saveButton.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("AGE", ageInput.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+        }
+    }
+}
